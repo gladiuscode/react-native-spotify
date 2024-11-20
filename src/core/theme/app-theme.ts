88e4872c827d2c9f4737ebeb9e129d3584487243
gladius <gladiusservices@protonmail.com>
2024-11-20 21:23:32 +0100

    feat: configure fonts and basic default styles

--- src/core/theme/app-theme.ts
+++ src/core/theme/app-theme.ts
@@ -1,3 +1,13 @@
+import {StyleSheet} from "react-native";
+
+const fontFamilies = {
+  roboto: {
+    regular: 'Roboto-Regular',
+    medium: 'Roboto-Medium',
+    bold: 'Roboto-Bold',
+  }
+}
+
 const AppTheme = {
   colors: {
     white: '#FFFFFF',
@@ -6,6 +16,31 @@ const AppTheme = {
     black: '#121212',
     green: '#1ED760',
   },
+  font: {
+    families: fontFamilies,
+    styles: StyleSheet.create({
+      title: {
+        fontFamily: fontFamilies.roboto.bold,
+        fontSize: 28,
+      },
+      h1: {
+        fontFamily: fontFamilies.roboto.bold,
+        fontSize: 20,
+      },
+      h2: {
+        fontFamily: fontFamilies.roboto.bold,
+        fontSize: 16,
+      },
+      text: {
+        fontFamily: fontFamilies.roboto.medium,
+        fontSize: 10,
+      },
+      small: {
+        fontFamily: fontFamilies.roboto.medium,
+        fontSize: 8,
+      },
+    })
+  }
 }
 
 export default AppTheme;
