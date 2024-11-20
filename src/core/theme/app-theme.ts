450f5284059dc9620ace0c46889616abfc985a1f
gladius <gladiusservices@protonmail.com>
2024-11-20 21:25:47 +0100

    feat: set default text color to white

--- src/core/theme/app-theme.ts
+++ src/core/theme/app-theme.ts
@@ -1,5 +1,13 @@
 import {StyleSheet} from "react-native";
 
+const colors = {
+  white: '#FFFFFF',
+  grey: '#777777',
+  darkGrey: '#282828',
+  black: '#121212',
+  green: '#1ED760',
+}
+
 const fontFamilies = {
   roboto: {
     regular: 'Roboto-Regular',
@@ -9,35 +17,34 @@ const fontFamilies = {
 }
 
 const AppTheme = {
-  colors: {
-    white: '#FFFFFF',
-    grey: '#777777',
-    darkGrey: '#282828',
-    black: '#121212',
-    green: '#1ED760',
-  },
+  colors,
   font: {
     families: fontFamilies,
     styles: StyleSheet.create({
       title: {
         fontFamily: fontFamilies.roboto.bold,
         fontSize: 28,
+        color: colors.white,
       },
       h1: {
         fontFamily: fontFamilies.roboto.bold,
         fontSize: 20,
+        color: colors.white,
       },
       h2: {
         fontFamily: fontFamilies.roboto.bold,
         fontSize: 16,
+        color: colors.white,
       },
       text: {
         fontFamily: fontFamilies.roboto.medium,
         fontSize: 10,
+        color: colors.white,
       },
       small: {
         fontFamily: fontFamilies.roboto.medium,
         fontSize: 8,
+        color: colors.white,
       },
     })
   }
