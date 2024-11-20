cfd2fe497370423d542891f68d38ae28c8e462ac
gladius <gladiusservices@protonmail.com>
2024-11-20 21:11:54 +0100

    feat: create appTheme colors

---
+++ src/core/theme/app-theme.ts
@@ -0,0 +1,11 @@
+const AppTheme = {
+  colors: {
+    white: '#FFFFFF',
+    grey: '#777777',
+    darkGrey: '#282828',
+    black: '#121212',
+    green: '#1ED760',
+  },
+}
+
+export default AppTheme;
