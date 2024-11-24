0c5348308484fa44d9d64dbfd876c07e607ade90
gladius <gladiusservices@protonmail.com>
2024-11-24 20:10:01 +0100

    feat: configure onlineManager and focusManager

--- src/core/fetch/app-fetch.tsx
+++ src/core/fetch/app-fetch.tsx
@@ -1,8 +1,31 @@
-import { QueryClientProvider } from '@tanstack/react-query';
+import { AppState, AppStateStatus } from 'react-native';
+import { PropsWithChildren, useEffect } from 'react';
+import {
+  QueryClientProvider,
+  onlineManager,
+  focusManager,
+} from '@tanstack/react-query';
+import NetInfo from '@react-native-community/netinfo';
 import appQueryClient from '@/core/fetch/init';
-import { PropsWithChildren } from 'react';
+
+// https://tanstack.com/query/v5/docs/framework/react/react-native#online-status-management
+onlineManager.setEventListener((setOnline) => {
+  return NetInfo.addEventListener((state) => {
+    setOnline(!!state.isConnected);
+  });
+});
+
+// https://tanstack.com/query/v5/docs/framework/react/react-native#refetch-on-app-focus
+function setIsFocused(status: AppStateStatus) {
+  focusManager.setFocused(status === 'active');
+}
 
 function AppFetch({ children }: PropsWithChildren) {
+  const subscription = AppState.addEventListener('change', setIsFocused);
+  useEffect(() => {
+    return () => subscription.remove();
+  }, []);
+
   return (
     <QueryClientProvider client={appQueryClient}>
       {children}
