e8c76b8582d81a8a0135fd8acc05f86104f6c93b
gladius <gladiusservices@protonmail.com>
2024-11-24 19:05:58 +0100

    feat: enhance useAppTranslation with additional helpers

--- src/core/i18n/use-app-translation.ts
+++ src/core/i18n/use-app-translation.ts
@@ -1,7 +1,9 @@
+import { useCallback } from 'react';
 import { useTranslation, UseTranslationOptions } from 'react-i18next';
 import type { FallbackNs } from 'react-i18next';
 import type { FlatNamespace, KeyPrefix } from 'i18next';
 import type { $Tuple } from 'react-i18next/helpers';
+import AppI18nConfig from '@/core/i18n/config';
 
 const useAppTranslation = <
   Ns extends FlatNamespace | $Tuple<FlatNamespace> | undefined = undefined,
@@ -10,7 +12,19 @@ const useAppTranslation = <
   ns?: Ns,
   options?: UseTranslationOptions<KPrefix>,
 ) => {
-  return useTranslation(ns, options);
+  const [t, i18n] = useTranslation(ns, options);
+
+  const changeLanguage = useCallback(
+    (value: (typeof AppI18nConfig.supportedLocales)[number]) =>
+      i18n.changeLanguage(value),
+    [i18n],
+  );
+
+  return {
+    t,
+    currentLanguage: i18n.language,
+    changeLanguage,
+  };
 };
 
 export default useAppTranslation;
