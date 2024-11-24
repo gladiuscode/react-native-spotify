1cdb8694a6d769fa36a014bea38ba4b86238d369
gladius <gladiusservices@protonmail.com>
2024-11-24 16:10:24 +0100

    feat: create common translation

--- src/core/i18n/app-i18n.ts
+++ src/core/i18n/app-i18n.ts
@@ -4,8 +4,11 @@ import AuthI18n from '@/features/auth/i18n/auth-i18n';
 import HomeI18n from '@/features/home/i18n/home-i18n';
 import languageDetector from '@/core/i18n/language-detector/language-detector';
 
+import commonEn from '@/core/i18n/translations/common.json';
+
 const resources = {
   en: {
+    common: commonEn,
     auth: AuthI18n.en,
     home: HomeI18n.en,
   },
