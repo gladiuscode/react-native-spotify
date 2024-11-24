0af1fd1519f0ca883e897d205f5bc3e9e91bb96b
gladius <gladiusservices@protonmail.com>
2024-11-24 16:12:39 +0100

    feat: create config

--- src/core/i18n/app-i18n.ts
+++ src/core/i18n/app-i18n.ts
@@ -1,30 +1,9 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import AuthI18n from '@/features/auth/i18n/auth-i18n';
-import HomeI18n from '@/features/home/i18n/home-i18n';
-import languageDetector from '@/core/i18n/language-detector/language-detector';
+import { PropsWithChildren } from 'react';
 
-import commonEn from '@/core/i18n/translations/common.json';
-import AppI18nDefaults from '@/core/i18n/defaults';
+import './config.ts';
 
-const resources = {
-  en: {
-    common: commonEn,
-    auth: AuthI18n.en,
-    home: HomeI18n.en,
-  },
-};
+function AppI18n({ children }: PropsWithChildren) {
+  return children;
+}
 
-i18n
-  .use(initReactI18next)
-  .use(languageDetector)
-  .init({
-    defaultNS: AppI18nDefaults.ns,
-    ns: ['common', 'auth', 'home'],
-    resources,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
-
-export default i18n;
+export default AppI18n;
