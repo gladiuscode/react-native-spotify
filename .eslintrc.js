a08ff670379240852ef483fdcad020126f896718
gladius <gladiusservices@protonmail.com>
2024-11-24 19:15:43 +0100

    feat: configure react-query plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'plugin:@tanstack/query/recommended'],
   rules: {
     'prettier/prettier': 'error',
   },
