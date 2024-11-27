2bef60918c2b7af6e9e4ffe9a9fd2c9b56b20773
gladius <gladiusservices@protonmail.com>
2024-11-27 23:29:22 +0100

    feat: add navigation back in NavHeader component

--- src/components/nav-header/nav-header.component.tsx
+++ src/components/nav-header/nav-header.component.tsx
@@ -1,18 +1,35 @@
-import { StyleSheet, View, ViewProps } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, ViewProps } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import React from 'react';
+import React, { useCallback } from 'react';
 import AppText from '@/components/app-typography/app-text.component';
 import appTheme from '@/core/theme/app-theme';
+import { useNavigation } from '@react-navigation/native';
+import { defaultHitSlop } from '@/core/constants';
 
 type Props = ViewProps & {
   text: string;
 };
 
+// TODO: ADD ANIMATION ON SWIPE BACK
 function NavHeader({ style, text, ...otherProps }: Props) {
+  const navigation = useNavigation();
+
+  const handleGoBack = useCallback(() => {
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+    }
+  }, []);
+
   return (
     <View style={[styles.container, style]} {...otherProps}>
       <View style={styles.expand}>
-        <Ionicons name="chevron-back" size={24} color={appTheme.colors.white} />
+        <TouchableOpacity hitSlop={defaultHitSlop} onPress={handleGoBack}>
+          <Ionicons
+            name="chevron-back"
+            size={24}
+            color={appTheme.colors.white}
+          />
+        </TouchableOpacity>
       </View>
       <AppText.Title style={styles.text}>{text}</AppText.Title>
       <View style={styles.expand} />
